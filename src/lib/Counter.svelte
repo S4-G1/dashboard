24042553176ef42291878532300b6a692bc36a9f
Kristian Nymann Jakobsen <kristian@nymann.dev>
2021-04-16 13:14:49 +0200

    :rotating_light: Fix linter warnings

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -5,19 +5,19 @@
 		count += 1;
 	};
 
-    const decrement = () => {
-        count -= 1;
-        alert("Contribute to github.com/S4-G1/dashboard")
-    };
+	const decrement = () => {
+		count -= 1;
+		alert('Contribute to github.com/S4-G1/dashboard');
+	};
 </script>
 
-    {#if count === 10}
-        <h1>We don't think so... do you?</h1>
-        <button on:click={decrement}>{count}</button>
-    {:else}
-        <h1>Can you reach 11?</h1>
-        <button on:click={increment}>{count}</button>
-    {/if}
+{#if count === 10}
+	<h1>We don't think so... do you?</h1>
+	<button on:click={decrement}>{count}</button>
+{:else}
+	<h1>Can you reach 11?</h1>
+	<button on:click={increment}>{count}</button>
+{/if}
 
 <style>
 	button {
