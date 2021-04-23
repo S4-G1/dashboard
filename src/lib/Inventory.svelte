b6d4c7ef453f98d89b3799807668daddb57b6687
Gamma <gammaishimwe@gmail.com>
2021-04-23 12:16:41 +0200

    Basic setup, without stylling

---
+++ src/lib/Inventory.svelte
@@ -0,0 +1,38 @@
+<script>
+	export let stock;
+</script>
+<style>
+    .Inventory{
+        border: solid black 3px;
+        padding: 30px;
+        height: 600px;
+        max-width: 600px;
+		margin-top: 40px;
+		margin-left: 75%;
+        overflow-y: scroll;
+    }
+
+    .stock {
+        border: solid grey 3px;
+        padding: 10px;
+        margin: 15px;
+        text-align: center;
+    }
+
+    /* .stockInner {
+		color:black;
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: row;
+    padding-bottom: 10px;
+	}
+
+	.stockInner > div {
+    } */
+</style>
+
+<div class="Inventory">
+	<div class="stock">
+		<slot {stock}></slot>	
+	</div>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,47 +1,11 @@
 <script>
-	import Counter from '$lib/Counter.svelte';
+	import Inventory from '$lib/Inventory.svelte';
+	
+	let stockList = ["mel","dej","hej","ko"];
 </script>
 
-<svelte:head>
-	<title>Hello world!</title>
-</svelte:head>
-
-<main>
-	<h1>Counter game</h1>
-
-	<Counter />
-</main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4rem;
-		font-weight: 100;
-		line-height: 1.1;
-		margin: 4rem auto;
-		max-width: 14rem;
-	}
-
-	p {
-		max-width: 14rem;
-		margin: 2rem auto;
-		line-height: 1.35;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			max-width: none;
-		}
-
-		p {
-			max-width: none;
-		}
-	}
-</style>
+<Inventory stocks={stockList} let:stock>
+	<div>
+		{stock}
+	</div>
+</Inventory>
