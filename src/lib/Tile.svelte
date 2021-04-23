f16c2c29e17efb62f6be42192497df6551957f3d
Kristian Nymann Jakobsen <kristian@nymann.dev>
2021-04-23 15:58:21 +0200

    :lipstick: Add tile panel as component
    body
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be ignored, and an empty message aborts the commit.
    #
    # On branch kpi-tile
    #
    # Commit title:
    #   :lipstick: Add tile panel as component
    #
    # Changes to be committed:
    #   #	modified:   .gitignore
    #	modified:   docker-compose.yml
    #	modified:   package.json
    #	modified:   src/lib/Tile.svelte
    #	modified:   src/routes/index.svelte
    #	new file:   yarn.lock
    #
    #

---
+++ src/lib/Tile.svelte
@@ -0,0 +1,47 @@
+<script>
+	export let icon;
+	export let value;
+	export let title;
+	export let selected = false;
+	console.log(icon);
+</script>
+
+<main class:selected on:click>
+	<svelte:component this={icon} />
+	<span class="title">{title}</span>
+	<span class="value">{value}</span>
+</main>
+
+<style>
+	main {
+		display: grid;
+		grid-template-columns: 1fr;
+		grid-template-rows: repeat(3, 1fr);
+		padding: 1em;
+		background: #d4d4d4;
+		user-select: none;
+		text-align: center;
+	}
+
+	:global(svg),
+	span {
+		display: grid;
+		place-self: center;
+	}
+
+	.title,
+	.value {
+		text-transform: uppercase;
+	}
+
+	.selected {
+		background: #388da6;
+		color: #ffffff;
+	}
+
+	.value {
+		font-weight: bold;
+		font-size: 28px;
+		margin: 0;
+	}
+</style>
