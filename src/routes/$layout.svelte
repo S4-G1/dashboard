bcb6614cde3b1057f800b480423028d1a74fe1c6
Emil <Thomsen>
2021-04-23 10:31:22 +0200

    add nav buttons

--- src/routes/$layout.svelte
+++ src/routes/$layout.svelte
@@ -3,8 +3,36 @@
 </script>
 
 <nav>
-	<a href=".">NotHome</a>
-	<a href="history">History</a>
-</nav>
+	<!--<a href=".">NotHome</a>
 
-<slot />
+	<a href="history">History</a>
+	-->
+
+<style>
+	button {
+		height: 4rem;
+		width: 8rem;
+		background-color: #aaa;
+		border-color: #f1c40f;
+		color: #f1c40f;
+		font-size: 1.25rem;
+		background-image: linear-gradient(45deg, #00ae00 50%, transparent 50%);
+		background-position: 100%;
+		background-size: 400%;
+		transition: background 300ms ease-in-out;
+	}
+	button:hover {
+		background-position: 0;
+		color: #0d26cf;
+	}
+</style>
+
+<button on:click>
+	NotHome
+</button>
+
+<button on:click>
+	History
+</button>
+
+</nav>
